3958fae811f382e88f283e4678e9ddf239cdff4f
Evan Stern <evanstern@machineservant-llc.com>
2019-09-30 10:57:35 -0400

    Add eslint and prettier configuration

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Application } from "express";
+import express, { Application, Request, Response } from 'express';
 
 const app: Application = express();
 
-app.get("/", (_req, res) => {
-  res.send("Hello World!");
+app.get('/', (_req: Request, res: Response) => {
+  res.send('Hello World!');
 });
 
 app.listen(8000, () => {
-  console.log("App is listening on port 8000!");
+  console.log('App is listening on port 8000!');
 });
