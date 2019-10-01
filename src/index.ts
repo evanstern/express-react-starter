2c3bae6776143c5524132863a4980dfac54e984e
Evan Stern <evanstern@machineservant-llc.com>
2019-10-01 17:52:41 -0400

    Fix mongoose connection, monitor tests

--- src/index.ts
+++ src/index.ts
@@ -7,6 +7,7 @@ const { PORT, MONGO_URI } = process.env;
 mongoose
   .connect(MONGO_URI!, {
     useNewUrlParser: true,
+    useUnifiedTopology: true,
   })
   .then(() => console.log('MongoDB Connected'))
   .catch(err => console.log(err));
